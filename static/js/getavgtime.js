01b5eeafe1ba89d67eb3cb0538a86c61679204c6
xhlgr <1850125838@qq.com>
2020-05-04 16:18:43 +0800

    Create getavgtime.js

---
+++ static/js/getavgtime.js
@@ -0,0 +1,25 @@
+<script>
+var data = {
+    api_key:'ur912507-8258182cfa5e7c2c21902bb9',
+    format:'json',
+    response_times:'1',//返回响应时间
+    response_times_limit:'1',//限制条数
+    response_times_average:'1'//返回24h平均响应时间
+};
+$.ajax({//获取uptimerobot监控的网站状态
+    method : 'POST',
+    url : 'https://api.uptimerobot.com/v2/getMonitors',
+    data : data,
+    dataType: "json",
+    success:function(data){//成功
+        //console.log(data);
+        if(data.stat=='ok'){//请求返回成功
+        	for(var i=0;i < data.monitors.length;i++){
+            var avgtime=Number(data.monitors[i].average_response_time).toFixed(2)//平均响应时间
+            document.getElementById("uptimerobot").innerHTML+=data.monitors[i].friendly_name+'首页平均响应时间：'+avgtime+'ms<br>';
+          }
+        }
+    },
+    error:function(textStatus){console.log("uptimerobot请求失败！");}
+});
+</script>
