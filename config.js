7d015c33f0b1a64a44a44d2ff94955b0756b31e1
xhlgr <1850125838@qq.com>
2020-05-04 11:35:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'Wybus网站状态',
 
   // 站点链接
   SiteUrl: '/',
@@ -19,17 +19,17 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 7,
 
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '主页',
+      url: 'https://www.wybus.xyz/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: '五邑交通维基',
+      url: 'https://w.wybus.xyz'
     }
   ]
 };
